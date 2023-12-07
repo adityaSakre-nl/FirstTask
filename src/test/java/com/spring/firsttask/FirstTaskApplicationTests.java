c6b1db57806de12f1f22f36bca5aecf574f838ac
adityaSakre-nl <152947120+adityaSakre-nl@users.noreply.github.com>
2023-12-07 14:33:38 +0530

    Initializing the spring-boot

---
+++ src/test/java/com/spring/firsttask/FirstTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.firsttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
